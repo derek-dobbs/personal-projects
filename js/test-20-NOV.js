6099826aac1de8a7119e1aab52b09dfded8df277
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2021-11-21 00:12:25 -0600

    Updated code

--- js/test-20-NOV.js
+++ js/test-20-NOV.js
@@ -3,8 +3,44 @@
 
 $(document).ready(function() {
 
-    $('.box-1').click(function() {
-        $(this).css('background-color', 'hotpink');
+    $('#box-1').click(function() {
+        $(this).css('background-color', 'red');
     });
+
+    $('#box-2').click(function() {
+        $(this).css('background-color', 'blue');
+    });
+
+    $('#box-3').click(function() {
+        $(this).css('background-color', 'green');
+    });
+
+    $('#box-4').click(function() {
+        $(this).css('background-color', 'yellow');
+    });
+
+    $('#julie-smile').click(function() {
+        $('#julie-pic-box').css('background-image', 'url("/img/julie-smile.jpeg")');
+        $('#dialouge-paragraph').html("Yayyy!");
+    });
+
+    $('#julie-frown').click(function() {
+        $('#julie-pic-box').css('background-image', 'url("/img/julie-frown.jpeg")');
+        $('#dialouge-paragraph').html("Boooooo!");
+    });
+
+    $('#julie-neutral').click(function() {
+        $('#julie-pic-box').css('background-image', 'url("/img/julie-neutral.jpeg")');
+        $('#dialouge-paragraph').html("Hello, I'm Julie!");
+    });
+
+
+
+
+
+
+
+
+
 
 });
